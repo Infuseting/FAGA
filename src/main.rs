7ba4e1afa3d3307716df1938f94c75e88c311404
Infuseting <infuseting@gmail.com>
2026-02-06 04:42:18 +0100

    reset coed

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::thread;
-use core::init;
 
+use std::thread;
 
 fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     log::info!("🚀 Start of FAGA Browser...");
+    log::info!("📱 Initialize UI Manager with Google-like interface...");
 
-    init();
 }
